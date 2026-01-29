fb84269e4073f5150293664e0dc36d2fc1d16899
Dadarzz2405 <haidaralifawwaz@gmail.com>
2026-01-29 08:23:05 +0700

    fixup!

--- models.py
+++ models.py
@@ -4,6 +4,8 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 class Admin(db.Model, UserMixin):
+    __tablename__ = 'admins'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     username = db.Column(db.String(150), unique=True, nullable=False)
@@ -11,36 +13,6 @@ class Admin(db.Model, UserMixin):
 
     def __repr__(self):
         return f'<Admin {self.username}>'
-
-class Captain(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), nullable=False)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
-    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<Captain {self.username} of House ID {self.house_id}>'
-
-class Member(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(150), nullable=False)
-    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<Member {self.name} - {self.role}>'
-
-class Advisor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(150), nullable=False)
-    bio = db.Column(db.Text)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
-    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'), nullable=False)
-    def __repr__(self):
-        return f'<Advisor {self.name}>'
 
 class House(db.Model):
     __tablename__ = 'houses'
@@ -51,12 +23,53 @@ class House(db.Model):
     house_points = db.Column(db.Integer, default=0)
 
     members = db.relationship('Member', backref='house', lazy=True)
+    captains = db.relationship('Captain', backref='house', lazy=True)
+    advisors = db.relationship('Advisor', backref='house', lazy=True)
     achievements = db.relationship('Achievement', backref='house', lazy=True)
 
     def __repr__(self):
         return f'<House {self.name}>'
 
+class Captain(db.Model, UserMixin):
+    __tablename__ = 'captains'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Captain {self.username} of House ID {self.house_id}>'
+
+class Member(db.Model):
+    __tablename__ = 'members'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(150), nullable=False)
+    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Member {self.name} - {self.role}>'
+
+class Advisor(db.Model):
+    __tablename__ = 'advisors'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(150), nullable=False)
+    bio = db.Column(db.Text)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    house_id = db.Column(db.Integer, db.ForeignKey('houses.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Advisor {self.name}>'
+
 class Achievement(db.Model):
+    __tablename__ = 'achievements'
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
     description = db.Column(db.Text)
